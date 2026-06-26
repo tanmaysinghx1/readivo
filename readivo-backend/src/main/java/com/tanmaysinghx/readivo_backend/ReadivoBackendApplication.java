4651d5f6d9ee50bf9a1f8ee086783af0d79552f8
Tanmay Singh <tanmaysinghx@gmail.com>
2026-06-26 14:50:35 +0530

    Repo Intilization

---
+++ readivo-backend/src/main/java/com/tanmaysinghx/readivo_backend/ReadivoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tanmaysinghx.readivo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadivoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadivoBackendApplication.class, args);
+	}
+
+}
---
+++ readivo-backend/src/test/java/com/tanmaysinghx/readivo_backend/ReadivoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmaysinghx.readivo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadivoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
